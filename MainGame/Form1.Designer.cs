15ae5859561eb9cfc86c8dfdd2e5621ce8a8d5ed
Hee-Ju Kang <112617468+HeeJuKang1027@users.noreply.github.com>
2024-05-10 01:54:33 +0900

    배경화면과 중간고사, 기말고사 이벤트 구현, 날짜 타이머 구현

---
+++ MainGame/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace MainGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bossSpawnTimer = new System.Windows.Forms.Timer(this.components);
+            this.textLabel = new System.Windows.Forms.Label();
+            this.textTimer = new System.Windows.Forms.Timer(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.StartTextTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelChangingText = new System.Windows.Forms.Label();
+            this.MiddleExamTimer = new System.Windows.Forms.Timer(this.components);
+            this.ChangeTextTimer = new System.Windows.Forms.Timer(this.components);
+            this.FinalExamTimer = new System.Windows.Forms.Timer(this.components);
+            this.bossSpawnTimer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MainGame.Properties.Resources.새빛관_1_600x400__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1485, 872);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bossSpawnTimer
+            // 
+            this.bossSpawnTimer.Interval = 52000;
+            this.bossSpawnTimer.Tick += new System.EventHandler(this.bossSpawnTimer_Tick);
+            // 
+            // textLabel
+            // 
+            this.textLabel.AutoSize = true;
+            this.textLabel.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textLabel.Location = new System.Drawing.Point(210, 338);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(990, 72);
+            this.textLabel.TabIndex = 1;
+            this.textLabel.Text = "대충 보스몹 뜨기전 플레이 중";
+            // 
+            // textTimer
+            // 
+            this.textTimer.Interval = 3000;
+            this.textTimer.Tick += new System.EventHandler(this.textTimer_Tick);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(591, 678);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(128, 64);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "시작";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // StartTextTimer
+            // 
+            this.StartTextTimer.Interval = 1000;
+            this.StartTextTimer.Tick += new System.EventHandler(this.StartTextTimer_Tick);
+            // 
+            // labelChangingText
+            // 
+            this.labelChangingText.AutoSize = true;
+            this.labelChangingText.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelChangingText.Location = new System.Drawing.Point(12, 9);
+            this.labelChangingText.Name = "labelChangingText";
+            this.labelChangingText.Size = new System.Drawing.Size(97, 40);
+            this.labelChangingText.TabIndex = 3;
+            this.labelChangingText.Text = "날짜";
+            // 
+            // MiddleExamTimer
+            // 
+            this.MiddleExamTimer.Interval = 10000;
+            this.MiddleExamTimer.Tick += new System.EventHandler(this.MiddleExamTimer_Tick);
+            // 
+            // ChangeTextTimer
+            // 
+            this.ChangeTextTimer.Interval = 1000;
+            this.ChangeTextTimer.Tick += new System.EventHandler(this.ChangeTextTimer_Tick);
+            // 
+            // FinalExamTimer
+            // 
+            this.FinalExamTimer.Interval = 10000;
+            this.FinalExamTimer.Tick += new System.EventHandler(this.FinalExamTimer_Tick);
+            // 
+            // bossSpawnTimer2
+            // 
+            this.bossSpawnTimer2.Interval = 52000;
+            this.bossSpawnTimer2.Tick += new System.EventHandler(this.bossSpawnTimer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1480, 869);
+            this.Controls.Add(this.labelChangingText);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer bossSpawnTimer;
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.Timer textTimer;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer StartTextTimer;
+        private System.Windows.Forms.Label labelChangingText;
+        private System.Windows.Forms.Timer MiddleExamTimer;
+        private System.Windows.Forms.Timer ChangeTextTimer;
+        private System.Windows.Forms.Timer FinalExamTimer;
+        private System.Windows.Forms.Timer bossSpawnTimer2;
+    }
+}
+
---
+++ MainGame/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainGame
+{
+    public partial class Form1 : Form
+    {
+        private string[] changingTexts = { "3월 2일", "3월 3일", "3월 4일", "3월 5일", "3월 6일", "3월 7일", "3월 8일", 
+                                           "3월 9일", "3월 10일","3월 11일", "3월 12일", "3월 13일", "3월 14일", "3월 15일",
+                                           "3월 16일", "3월 17일", "3월 18일", "3월 19일", "3월 20일", "3월 21일", "3월 22일",
+                                           "3월 23일", "3월 24일","3월 25일", "3월 26일", "3월 27일", "3월 28일", "3월 29일",
+                                           "3월 30일", "3월 31일", "4월 1일", "4월 2일", "4월 3일", "4월 4일", "4월 5일", "4월 6일",
+                                           "4월 7일", "4월 8일", "4월 9일", "4월 10일", "4월 11일", "4월 12일", "4월 13일", "4월 14일",
+                                           "4월 15일","4월 16일", "4월 17일", "4월 18일", "4월 19일", "4월 20일", "4월 21일", "4월 22일\n중간고사",
+                                           "4월 22일\n중간고사","4월 22일\n중간고사","4월 22일\n중간고사"};
+        private int changingTextIndex = 0;
+
+        private string[] changingTexts2 = {"4월 23일", "4월 23일", "4월 23일", "4월 24일", "4월 25일", "4월 26일", "4월 27일", "4월 28일", "4월 29일",
+                                           "4월 30일", "5월 1일","5월 2일", "5월 3일", "5월 4일", "5월 5일", "5월 6일",
+                                           "5월 7일", "5월 8일", "5월 9일", "5월 10일", "5월 11일", "5월 12일", "5월 13일",
+                                           "5월 14일", "5월 15일","5월 16일", "5월 17일", "5월 18일", "5월 19일", "5월 20일",
+                                           "5월 21일", "5월 22일", "5월 23일", "5월 24일", "5월 25일", "5월 26일", "5월 27일", "5월 28일",
+                                           "5월 29일", "5월 30일", "5월 31일", "6월 1일", "6월 2일", "6월 3일", "6월 4일", "6월 5일",
+                                           "6월 6일","6월 7일", "6월 8일", "6월 9일", "6월 10일\n기말고사",
+                                           "6월 10일\n기말고사","6월 10일\n기말고사","6월 10일\n기말고사"};
+        private int changingTextIndex2 = 0;
+
+        private bool isMExamFinished = false;
+        private bool isFExamFinished = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 타이머 초기화
+            bossSpawnTimer = new Timer();
+            bossSpawnTimer.Interval = 52000; // 52초
+            bossSpawnTimer.Tick += bossSpawnTimer_Tick;
+
+            // 타이머2 초기화
+            bossSpawnTimer2 = new Timer();
+            bossSpawnTimer2.Interval = 52000; // 52초
+            bossSpawnTimer2.Tick += bossSpawnTimer2_Tick;
+
+            textTimer = new Timer();
+            textTimer.Interval = 3000; // 3초
+            textTimer.Tick += textTimer_Tick;
+
+            // 시작 텍스트 타이머 초기화
+            StartTextTimer = new Timer();
+            StartTextTimer.Interval = 1000; // 1초
+            StartTextTimer.Tick += StartTextTimer_Tick;
+
+            // 중간고사 타이머
+            MiddleExamTimer = new Timer();
+            MiddleExamTimer.Interval = 10000; // 10초
+            MiddleExamTimer.Tick += MiddleExamTimer_Tick;
+
+            //중간고사 이후 텍스트 변경 타이머
+            ChangeTextTimer = new Timer() { Interval = 1000 };
+            ChangeTextTimer.Tick += ChangeTextTimer_Tick;
+
+            // 기말고사 타이머
+            FinalExamTimer = new Timer();
+            FinalExamTimer.Interval = 10000; // 10초
+            FinalExamTimer.Tick += FinalExamTimer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 게임 시작 시 타이머는 작동되지 않음
+            bossSpawnTimer.Stop();
+            textTimer.Stop();
+            StartTextTimer.Stop();
+        }
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            bossSpawnTimer.Start(); //50초 세는중
+
+            buttonStart.Visible = false;
+
+            // 시작 텍스트 타이머 시작
+            StartTextTimer.Start(); // 텍스트 바뀌는중
+        }
+        private void bossSpawnTimer_Tick(object sender, EventArgs e)
+        {
+            // 보스 몬스터 생성 전에 '중간고사 시작' 텍스트 출력
+            ShowText("중간고사 시작", 3000);
+            bossSpawnTimer.Stop();
+        }
+
+        private void textTimer_Tick(object sender, EventArgs e)
+        {
+            // TextTimer가 시작되면 StartTextTimer를 멈춤
+            StartTextTimer.Stop();
+            labelChangingText.Visible = true;
+            // 3초 후에 텍스트 숨기기
+            textLabel.Visible = false;
+            textTimer.Stop();
+
+            MiddleExamTimer.Start();
+   
+
+        }
+
+        private void StartTextTimer_Tick(object sender, EventArgs e)
+        {
+            labelChangingText.Text = changingTexts[changingTextIndex];
+            changingTextIndex = (changingTextIndex + 1) % changingTexts.Length;
+
+        }
+
+        private void MiddleExamTimer_Tick(object sender, EventArgs e)
+        {
+            if (!isMExamFinished)
+            {
+                ShowText("중간고사 종료", 3000);
+                isMExamFinished = true;
+                bossSpawnTimer2.Start();
+
+                // 중간고사 종료 후에 텍스트 변경 타이머 시작
+                ChangeTextTimer.Start();
+            }
+        }
+        private void ChangeTextTimer_Tick(object sender, EventArgs e)
+        {
+            labelChangingText.Text = changingTexts2[changingTextIndex2];
+            changingTextIndex2 = (changingTextIndex2 + 1) % changingTexts2.Length;
+        }
+
+        private void ShowText(string text, int duration)
+        {
+            textLabel.Text = text;
+            textLabel.Font = new System.Drawing.Font("Arial", 40, System.Drawing.FontStyle.Bold);
+            textLabel.ForeColor = System.Drawing.Color.Black;
+            textLabel.AutoSize = true;
+            textLabel.Left = (ClientSize.Width - textLabel.Width) / 2;
+            textLabel.Top = (ClientSize.Height - textLabel.Height) / 2;
+            textLabel.Visible = true;
+
+            textTimer.Start();
+        }
+
+        private void bossSpawnTimer2_Tick(object sender, EventArgs e)
+        {
+            // 보스 몬스터 생성 전에 '기말고사 시작' 텍스트 출력
+            ShowText("기말고사 시작", 3000);
+            FinalExamTimer.Start();
+            bossSpawnTimer2.Stop();
+            ChangeTextTimer.Stop();
+        }
+        private void FinalExamTimer_Tick(object sender, EventArgs e)
+        {
+            // 종강 텍스트 출력
+            ShowText("종강", 3000);
+            FinalExamTimer.Stop();
+        }
+
+       
+    }
+}
