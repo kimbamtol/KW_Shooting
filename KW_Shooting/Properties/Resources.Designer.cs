d4f6f3deaf277258d3b16f8bc6b2a70549de6b62
태훈 김 <kimbamtol0304@gmail.com>
2024-06-14 22:23:03 +0900

    패널 추가 및 좌표수정

--- KW_Shooting/Properties/Resources.Designer.cs
+++ KW_Shooting/Properties/Resources.Designer.cs
@@ -141,6 +141,16 @@ namespace KW_Shooting.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap pixelheart {
+            get {
+                object obj = ResourceManager.GetObject("pixelheart", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap shooting {
             get {
                 object obj = ResourceManager.GetObject("shooting", resourceCulture);
