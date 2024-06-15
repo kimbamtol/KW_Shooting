f8814908068ebf154512f9d8f9ecee974ccb68fa
태훈 김 <kimbamtol0304@gmail.com>
2024-06-15 22:56:22 +0900

    스페셜 몬스터 + 라이프 감소 로직 추가

--- KW_Shooting/Properties/Resources.Designer.cs
+++ KW_Shooting/Properties/Resources.Designer.cs
@@ -186,5 +186,15 @@ namespace KW_Shooting.Properties {
                 return ResourceManager.GetStream("SkillQSound", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap Special {
+            get {
+                object obj = ResourceManager.GetObject("Special", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
